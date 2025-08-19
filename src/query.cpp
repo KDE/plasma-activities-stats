KDE/plasma-activities-stats

commit e6f30c5a5afda87bfa70466f6fd53a18597b741b

Query: Simplify code using isEmpty

src/query.cpp
@@ -135,7 +135,7 @@ bool Query::operator!=(const Query &right) const
                                                                                                                                                                \
     QStringList Query::What() const                                                                                                                            \
     {                                                                                                                                                          \
-        return d->What.size() ? d->What : Default;                                                                                                             \
+        return d->What.isEmpty() ? d->What : Default;                                                                                                          \
     }                                                                                                                                                          \
                                                                                                                                                                \
     void Query::clear##WHAT()                                                                                                                                  \

commit b5a887b8a3d96c91fcff7bb0e7f199546a1e7804

Fix when the default value should be used
Bug introduced in e6f30c5a5afda87bfa70466f6fd53a18597b741b -- replacing
size() with isEmpty() inverted the condition.

src/query.cpp
@@ -135,7 +135,7 @@ bool Query::operator!=(const Query &right) const
                                                                                                                                                                \
     QStringList Query::What() const                                                                                                                            \
     {                                                                                                                                                          \
-        return d->What.isEmpty() ? d->What : Default;                                                                                                          \
+        return d->What.isEmpty() ? Default : d->What;                                                                                                          \
     }                                                                                                                                                          \
                                                                                                                                                                \
     void Query::clear##WHAT()                                                                                                                                  \